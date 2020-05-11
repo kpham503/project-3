b8a460369089c6f8b0e982e8a2c505d26559af75
Kevin Pham <kevinpham@Kevins-MacBook-Pro-3.local>
2020-05-11 15:26:40 -0700

    Add changes to dog-gallery.js and dogs-gallery.html

---
+++ client/javascripts/functions.js
@@ -0,0 +1,48 @@
+// esversion 6
+
+/fill an array with the numbers from start to stop, in steps of step
+let fillArray = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+
+//create an array of the numbers 1 - 100
+let arrOf1To100 = fillArray(1, 100, 1);
+
+//log the array for inspection
+console.log(arrOf1To100);
+
+let bitBatBotOrNot = (n) => {
+   //your code goes here
+   let result = "";
+   if (n % 3 == 0){
+     result = `${result}Bit`;
+   }
+};
+
+let findAllbitBatBotOrNots1 = (arr) => {
+   //your code goes here
+   //use map method
+}
+
+let findAllbitBatBotOrNots2 = (arr) => {
+   //your code goes here
+   //use for statement
+}
+
+let findAllbitBatBotOrNots3 = (arr) => {
+   //your code goes here
+   //use for..of statement
+}
+
+let findAllbitBatBotOrNots4 = (arr) => {
+   //your code goes here
+   //use forEach method
+}
+
+//test the functions
+
+findAllbitBatBotOrNots1(arrOf1To100)
+
+findAllbitBatBotOrNots2(arrOf1To100)
+
+findAllbitBatBotOrNots3(arrOf1To100)
+
+findAllbitBatBotOrNots4(arrOf1To100)
